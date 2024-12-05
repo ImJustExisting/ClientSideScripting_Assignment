19426eed35e2335d594f9b3f7883706b8684a597
ImJustExisting <159423284+ImJustExisting@users.noreply.github.com>
2024-12-06 00:57:16 +0100

    Endpoint links added as well as remineders/notes for myself later

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+    ENDPOINTS USED
+    Clues: http://cluebase.lukelav.in/clues
+    Categories: http://cluebase.lukelav.in/categories
+
+    use fetch function
+
+    ask if each endpoint requires a seperate JS file or not
+*/
