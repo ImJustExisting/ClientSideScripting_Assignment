41b7db9b8a20226b26acb1a65c19b14a482055f5
ImJustExisting <159423284+ImJustExisting@users.noreply.github.com>
2024-12-06 22:18:44 +0100

    added logo

--- script.js
+++ script.js
@@ -5,5 +5,23 @@
 
     use fetch function
 
-    ask if each endpoint requires a seperate JS file or not
 */
+
+// Select the root element in the HTML where we will append our content
+const app = document.getElementById('root');
+
+// Create an image element for the logo and set its source
+const logo = document.createElement('img');
+logo.src = 'Jeopardy!_logo.png';
+
+// Create a container div to hold the movie cards, set its class for styling
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+// Append the logo and container to the root element
+app.appendChild(logo);
+app.appendChild(container);
+
+
+fetch('http://cluebase.lukelav.in/clues')
+fetch('http://cluebase.lukelav.in/categories')
